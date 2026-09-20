aa32c44c5b79afc493b971bc7961947bd0217ef0
Nastiti Ayu Lestari <nastitiayu100@gmail.com>
2026-09-20 22:47:45 +0700

    Labwork 1 -Prelab

---
+++ src/lw01/prelab/Main.java
@@ -0,0 +1,34 @@
+package lw01.prelab;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<PrintJob> jobs = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File("jobs.txt"))) {
+            while (scanner.hasNext()) {
+                String type = scanner.next();
+                String id = scanner.next();
+                int pages = scanner.nextInt();
+
+                if (type.equals("MONO")) {
+                    jobs.add(new MonoPrint(id, pages));
+                } else if (type.equals("COLOUR")) {
+                    jobs.add(new ColourPrint(id, pages));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("jobs.txt not found: " + e.getMessage());
+            return;
+        }
+
+        for (PrintJob job : jobs) {
+            System.out.println(job.summary());
+        }
+    }
+}
